70d3c8450926b7588c421d988384750662761885
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-04-01 13:42:32 +0000

    Second commit, added html files and connected to both flask and mongodb

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import env
 
 app = Flask(__name__)
 
 
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = 'task_manager'
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello, World...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
